a2566d9f0a601c9e22d913b523cba8bbb27ecdc7
Jean Rouge <jer329@cornell.edu>
2017-01-22 19:04:20 -0800

    WIP

--- erts/emulator/beam/bif.c
+++ erts/emulator/beam/bif.c
@@ -8,7 +8,9 @@
 #include "global.h"
 #include "erl_process.h"
 #include "big.h"
+#define ERL_WANT_HIPE_BIF_WRAPPER__
 #include "bif.h"
+#undef ERL_WANT_HIPE_BIF_WRAPPER__
 #include "beam_catches.h"
 #include "erl_debug.h"
 #include "erl_map.h"
@@ -227,9 +229,7 @@ pps(Process* p, Eterm* stop)
 #include "global.h"
 #include "erl_process.h"
 #include "error.h"
-#define ERL_WANT_HIPE_BIF_WRAPPER__
-#include "bif.h"
-#undef ERL_WANT_HIPE_BIF_WRAPPER__
+
 #include "big.h"
 #include "dist.h"
 #include "erl_version.h"
