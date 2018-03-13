3d758a6caf949ccd5a5e0d10c27db9f99852d65f
Jean Rouge <jer329@cornell.edu>
2018-03-13 12:36:14 -0700

    WIP

--- erts/emulator/beam/erl_hm.c
+++ erts/emulator/beam/erl_hm.c
@@ -6,11 +6,17 @@
 #include "erl_vm.h"
 #include "global.h"
 #include "erl_process.h"
+#include "error.h"
+#include "bif.h"
+#include "erl_binary.h"
+
 #define ERL_WANT_HIPE_BIF_WRAPPER__
 #include "bif.h"
 #undef ERL_WANT_HIPE_BIF_WRAPPER__
 
-BIF_RETTYPE hm_new_0(BIF_ALIST_2){
-  Eterm* result = erl_mk_int(42);
-  BIF_RET(*result);
+BIF_RETTYPE hm_new_0(BIF_ALIST_0){
+  BIF_RET(am_ok);
 }
+
+
+
