05a8db6c4eeb518363cb0e4545fe6ead47ff98ce
Jean Rouge <jer329@cornell.edu>
2018-03-14 00:52:24 -0700

    WIP

--- erts/emulator/beam/erl_hm.c
+++ erts/emulator/beam/erl_hm.c
@@ -88,12 +88,13 @@ BIF_RETTYPE hm_new_0(BIF_ALIST_0) {
 
 BIF_RETTYPE hm_set_3(BIF_ALIST_3) {
   hashmap_t* hm = (hashmap_t*)hm_val(BIF_ARG_3);
+  Eterm bordel = mkatom("bordel");
 
   Uint32 hash = 12; // TODO wkpo hashmap_make_hash(BIF_ARG_1);
   // TODO wkpo ca marche pas cette histoire la, collisions?
   WK_DEBUG("avant set %d", hm->j_array);
   // JLI(BIF_ARG_2, hm->j_array, hash); // TODO wkpo
-  JLI(mkatom("bordel"), hm->j_array, hash);
+  JLI(bordel, hm->j_array, hash);
   WK_DEBUG("apres set %d", hm->j_array);
 
   BIF_RET(BIF_ARG_3);
